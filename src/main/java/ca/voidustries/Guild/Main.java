4a8f4613eb6b2ae98e71245878158a50c97e653d
StoneMonarch <joshleubecherrobinson@gmail.com>
2021-01-12 14:43:49 -0500

    Small optimization to the hex string rendering
    Removed unneeded steps along with unnecessary casting of data types

--- src/main/java/ca/voidustries/Guild/Main.java
+++ src/main/java/ca/voidustries/Guild/Main.java
@@ -9,7 +9,6 @@ import org.joda.time.DateTimeZone;
 import org.joda.time.Period;
 import org.joda.time.PeriodType;
 
-import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -32,19 +31,17 @@ public class Main {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                DateTime endTime = new DateTime(2021, 3, 11, 0, 0);
-                DateTime startTime = new DateTime();
+                DateTime endTime = new DateTime(2021, 3, 11, 0, 0, DateTimeZone.forOffsetHours(-5));
+                DateTime startTime = new DateTime(DateTimeZone.forOffsetHours(-5));
                 Period p = new Period(startTime, endTime, PeriodType.yearDayTime());
                 String activityString = "Days: " + p.getDays() + ", Hours: " + p.getHours();
-                String hex = "";
+                StringBuilder hex = new StringBuilder();
                 for (int i = 0; i < activityString.length(); i++) {
-                    char ch = activityString.charAt(i);
-                    int in = (int)ch;
-                    String part = Integer.toHexString(in);
-                    hex += part;
+                    hex.append(Integer.toHexString(activityString.charAt(i)));
                 }
 
-                api.updateActivity(hex);
+                System.out.println(hex);
+                api.updateActivity(hex.toString());
             }
         }, 0, 3600000);
     }
